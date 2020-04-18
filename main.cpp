16f3872a3e8f850d27e9894336b4e417ccdedf9c
Francisco Jose Nasif <franciscojnasif@gmail.com>
2020-04-18 16:06:07 -0300

    Version 0.0.2

--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@ const int ALTO_PANTALLA = 600;
 
 int main(){
     if(!inicializar()) printf("Fallo la inicializacion de SDL\n");
+    SDL_FillRect(gSurface, NULL, SDL_MapRGB(gSurface->format, 0xFF, 0xFF, 0xFF ));
+    SDL_UpdateWindowSurface(gWindow);
     terminar();
 }
 
