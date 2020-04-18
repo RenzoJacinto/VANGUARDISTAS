e44da65a1e2e1aee77719eb162ae2bcd2403b32f
Francisco Jose Nasif <franciscojnasif@gmail.com>
2020-04-18 15:59:56 -0300

    Version 0.0.1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ bool inicializar();
 void terminar();
 
 SDL_Window* gWindow = NULL;
-SDL_Surfce* gSurface = NULL;
+SDL_Surface* gSurface = NULL;
 
 const int ANCHO_PANTALLA = 800;
 const int ALTO_PANTALLA = 600;
@@ -17,14 +17,14 @@ int main(){
 
 bool inicializar(){
     bool ok = true;
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 ){
-        printf("No se pudo inicializar SDL, SDL_Error: %s\n", SDL_GetError)
+    if(SDL_Init( SDL_INIT_VIDEO ) < 0){
+        printf("No se pudo inicializar SDL, SDL_Error: %s\n", SDL_GetError);
         ok = false;
     } else{
         gWindow = SDL_CreateWindow("GLEY LANCER", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, ANCHO_PANTALLA, ALTO_PANTALLA, SDL_WINDOW_SHOWN );
-        if( gWindow == NULL ){
-			printf( "La ventana no pudo ser creada! SDL_Error: %s\n", SDL_GetError() );
-			success = false;
+        if(gWindow == NULL){
+			printf( "La ventana no pudo ser creada! SDL_Error: %s\n", SDL_GetError());
+			ok = false;
 		} else{
 			gSurface = SDL_GetWindowSurface(gWindow);
 		}
