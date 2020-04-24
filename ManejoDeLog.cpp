3b8093073421c59c36439ce7c4df7d9297debb7e
RenzoJacinto <renzojacinto12@hotmail.com>
2020-04-24 19:48:31 -0300

    crea el archivo log con la fecha y hora

--- ManejoDeLog.cpp
+++ ManejoDeLog.cpp
@@ -1,4 +1,5 @@
 #include "ManejoDeLog.h"
+//#include <string>
 
 ManejoDeLog::ManejoDeLog(){
     debug = true;
@@ -7,7 +8,12 @@ ManejoDeLog::ManejoDeLog(){
 }
 
 bool ManejoDeLog::iniciarLog(){
-    archivo_log.open ("log.txt", ios::out);
+    time_t ahora = time(0);
+    //const char* carpeta = "log/";
+    char* hora_actual = ctime(&ahora);
+    //std::string direccion = carpeta;
+    //direccion.append(hora_actual);
+    archivo_log.open (hora_actual, ios::out);
     if (!archivo_log){ return false;}
     return true;
 }
