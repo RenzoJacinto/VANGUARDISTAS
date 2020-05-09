2ac62f0d5dfd1d526629778b9dc89c6c76617274
Francisco Jose Nasif <franciscojnasif@gmail.com>
2020-05-09 17:24:38 -0300

    Agregue lo que habia dicho (el caso no tomado antes), si ahora se pasan ambos se queda con el mas grande, si son iguales no hay drama y elije cualquiera. Luego tambien es evaluado si ese nivel de log es correcto, lo cual tambien es un problema ya que si suponte que por parametro nos mandan 4 y por json 3, se queda con el 4 y da error (PROBLEMA A SOLUCIONAR)

--- main.cpp
+++ main.cpp
@@ -12,16 +12,22 @@ int main( int argc, char* argv[] ){
     json.abrir_archivo();
     int log_json = json.get_nivel_de_log();
     int nivel_log = 0;
+    int log_param = 0;
 
     //Si no se pasó un solo parámetro avisa al usuario
     if(argc != CANTIDAD_PARAMETROS_VALIDOS){
+        // No se paso ningun valor (x param ni x config.json)
         if(log_json == 0){
             printf("Cantidad de parámetros inválida");
             return 0;
         }
         nivel_log = log_json;
-    } else nivel_log = atoi(argv[1]);
-
+    } else{
+        log_param = atoi(argv[1]);
+        // Evalua cual es mayor (por ser niveles se queda con el mas grande)
+        if(log_param < log_json) nivel_log = log_json;
+        else nivel_log = log_param;
+    }
     Menu menu = Menu();
     //Si el parámetro pasado no es válido avisa al usuario
     if (!logger.iniciarLog(nivel_log)){
