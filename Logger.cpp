1fa148becabf71796c16f83e6a12569b7a2f8d5a
RenzoJacinto <renzojacinto12@hotmail.com>
2020-05-27 22:00:11 -0300

    hora

--- Logger.cpp
+++ Logger.cpp
@@ -1,6 +1,10 @@
 #include "Logger.h"
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <sys/time.h>
+#include <time.h>
+#include <math.h>
 using namespace std;
 
 Logger::Logger(){}
@@ -12,13 +16,31 @@ void Logger::info(const char* update){}
 void Logger::error(const char* update){}
 
 void Logger::escribir(const char* update){
-    time_t ahora = time(0);
-    char* hora_actual = ctime(&ahora);
+    char buffer[26];
+    int millisec;
+    struct tm* tm_info;
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+
+    millisec = lrint(tv.tv_usec/1000.0); // Round to nearest millisec
+    if (millisec>=1000) { // Allow for rounding up to nearest second
+        millisec -=1000;
+        tv.tv_sec++;
+    }
+
+    tm_info = localtime(&tv.tv_sec);
+
+    strftime(buffer, 26, "%d/%m/%Y %H:%M:%S.", tm_info);
+    std::string horaActual(buffer);
+    std:string ms = std::to_string(millisec);
+
     const char* espacio = " - ";
+    const char* new_line = "\n";
     string guion(espacio);
-    string horaActual(hora_actual);
     string sUpdate(update);
-    string hora = sUpdate + guion + horaActual;
+    string newLine(new_line);
+    string hora = horaActual + ms + guion + sUpdate + newLine;
     archivo_log << hora.c_str();
 }
 bool Logger::abrirArchivo(const char* filename){
