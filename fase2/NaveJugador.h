2007d6b1591442d2af17b9c34a18ac0c3abafab6
gmf92 <gmf92@hotmail.com>
2020-07-04 17:06:06 -0300

    .

--- fase2/NaveJugador.h
+++ fase2/NaveJugador.h
@@ -15,8 +15,8 @@ class NaveJugador: public Nave {
 
         //Las dimensiones de la Nave del Jugador
         int id_nave;
-		static const int NAVE_WIDTH = 50;
-		static const int NAVE_HEIGHT = 21;
+		static const int NAVE_WIDTH = 80;
+		static const int NAVE_HEIGHT = 34;
 
 		//Velocidad de la Nave del Jugador
 		static const int NAVE_VEL = 5;
