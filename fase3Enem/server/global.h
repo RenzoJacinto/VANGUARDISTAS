205bd14fda9eaaf89850031be1ac57008f5b7287
Francisco Jose Nasif <franciscojnasif@gmail.com>
2020-07-30 03:10:44 -0300

    borro la carpeta que habia creado

--- fase3Enem/server/global.h
+++
@@ -1,8 +0,0 @@
-#include "ManejoDeLog.h"
-#include "ManejoDeJson.h"
-#include <pthread.h>
-#include "typesmsj.h"
-
-extern ManejoDeLog logger;
-extern ManejoDeJson json;
-extern pthread_mutex_t mutex;
